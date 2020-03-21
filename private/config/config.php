529a03a095dc4f517cdffecb23c7a57d6d565a33
Vladi97jimenez <jiemenezpinedaeliezer@gmail.com>
2020-03-21 14:14:49 -0600

    listo

---
+++ private/config/config.php
@@ -0,0 +1,7 @@
+<?php 
+/**
+ * Importamos la clase principal de conexion
+ */
+include('../../conexion/BD.php');
+$conexion = new DB('localhost','root','','db_app_academica');
+?>
---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+var $ = el => document.querySelector(el);
+document.addEventListener("DOMContentLoaded", event => {
+   
+    let alumnos = document.getElementById("alumnos"); // Instanciamos los elementos
+    let docentes = document.getElementById("docentes"); //
+
+    //SE AGREGAN LOS EVENTOS LISTENER PARA CADA VISTA
+    alumnos.addEventListener("click", e=>{
+        e.stopPropagation();
+        let vista = "alumnos";
+        colocarVista(vista);
+
+    });
+
+    docentes.addEventListener("click", e=>{
+        e.stopPropagation();
+        let vista = "docentes";
+        colocarVista(vista);
+    
+    });
+}); 
+ function colocarVista(vista){
+
+    fetch(`public/vistas/${vista}/${vista}.html`).then( resp => resp.text()).then( resp => {
+
+        document.getElementById(`vistas-${vista}`).innerHTML = resp;
+        let btnCerrar = $(".close");
+
+        btnCerrar.addEventListener("click", event => {
+            $(`#vistas-${vista}`).innerHTML = "";
+        });
+
+        let cuerpo = $("body"), script = document.createElement("script");
+        script.src = `public/vistas/${vista}/${vista}.js`;
+        cuerpo.appendChild(script);
+            
+    });
+
+ }
